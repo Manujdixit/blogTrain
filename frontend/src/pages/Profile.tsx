0ff31b99a7ecb8024a581cf02b097ec648fc2526
Manujdixit <manujdixit04@gmail.com>
2024-07-30 16:04:19 +0530

    profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from "react";
 import { profileBlogs } from "../hooks";
-// import { Pagination } from "@nextui-org/react";
 import BlogCard from "../components/ProfileBlogs";
 
 export const Profile = () => {
   const [username, setusername] = useState("");
   const { loading, blogs } = profileBlogs();
-
-  // console.log(total);
-  // const handlePageChange = (pageNumber: number) => {
-  //   setcurrentpage(pageNumber);
-  // };
 
   useEffect(() => {
     const storedUsername = localStorage.getItem("username");
@@ -20,26 +14,21 @@ export const Profile = () => {
   }, []);
 
   return (
-    <div className="">
-      <div className="bg-[#0F0F10] min-h-screen flex gap-5 px-20">
-        <div className="w-1/2 bg-[#0F0F10] p-10 flex items-center justify-center">
+    <div>
+      <div className="bg-[#0F0F10] h-screen  flex ">
+        <div className="w-1/3 h-full bg-[#0F0F10] p-10 flex items-center justify-center">
           <div className="flex flex-col items-center pb-20">
-            <div className="">
-              <Avatar size="small" name={username} />
-            </div>
+            <Avatar size="small" name={username} />
             <div className="text-white text-4xl mt-4">Manuj Dixit</div>
+            <br />
+            {/* <h3 className=" text-green-500 cursor-pointer">Edit</h3> */}
           </div>
         </div>
-        <div className="w-3/4 bg-[#0F0F10] p-20 flex flex-col columns-2 gap-32">
+        <div className="overflow-auto bg-[#0F0F10] p-20 flex flex-col columns-2 gap-32">
           <div>
             <div className="text-white text-3xl ">About</div>
             <br />
-            <div className="text-white">
-              John Doe is a software engineer with a passion for building
-              innovative web applications. He has been working in the industry
-              for over 5 years and has a strong background in full-stack
-              development.
-            </div>
+            <div className="text-white">blogTrainer</div>
           </div>
           <div>
             <div className="text-white text-3xl "> Blog Posts</div>
