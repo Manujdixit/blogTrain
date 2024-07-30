b141c847e2a28a6dff232e889605d1495ae22440
Manujdixit <manujdixit04@gmail.com>
2024-07-30 16:39:11 +0530

    sign

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -21,14 +21,19 @@ import { Quote } from "../components/Quote";
 
 export const Signin = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full  px-4">
-        <div className="grid grid-cols-1 lg:grid-cols-2 ">
-          <div className="flex justify-center">
-            <Auth type="signin" />
-          </div>
-          <div className="hidden lg:flex lg:items-center lg:justify-center">
-            <Quote type="signin" />
+    <div className="flex flex-col items-center justify-center ">
+      <h1 className="text-gray-400 my-5 block lg:hidden text-[10px]">
+        BlogTrain
+      </h1>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-full  px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 ">
+            <div className="flex justify-center">
+              <Auth type="signin" />
+            </div>
+            <div className="hidden lg:flex lg:items-center lg:justify-center">
+              <Quote type="signin" />
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,7 +3,10 @@ import { Quote } from "../components/Quote";
 
 export const Signup = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center ">
+      <h1 className="text-gray-400 my-5 block lg:hidden text-[10px]">
+        BlogTrain
+      </h1>
       <div className="w-full  px-4">
         <div className="grid grid-cols-1 lg:grid-cols-2 ">
           <div className="flex justify-center">
